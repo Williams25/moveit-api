ee7ef3c0188411b9ec3fe39f9e2f76348b5744a6
Williams25 <william.souza.timao@hotmail.com>
2021-03-02 15:16:13 -0300

    Adicionando validações

--- src/controllers/MovesController.js
+++ src/controllers/MovesController.js
@@ -4,10 +4,12 @@ module.exports = {
   update: async (req, res) => {
     const { userId, level, currentExperience, challengesCompleted } = req.body
 
+    if (!userId || !level || !currentExperience || !challengesCompleted) return res.status(400).json({ message: 'Erro ao atualizar moves' })
+
     const move = await Moves.update({ level, currentExperience, challengesCompleted }, { where: { userId } })
 
     if (move != 1) return res.status(400).json({ message: 'Erro ao atualizar moves' })
 
-    return res.status(200).json({userId, level, currentExperience, challengesCompleted})
+    return res.status(200).json({ userId, level, currentExperience, challengesCompleted })
   },
 }
