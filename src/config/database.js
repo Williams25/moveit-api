35296d0f36f4d91060e9959142e2d4a1655cfaf1
Williams25 <william.souza.timao@hotmail.com>
2021-03-02 15:15:52 -0300

    Verificando que tipo de base de dados  deve ser utilizada tests ou produção

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,13 @@
+require('dotenv').config()
+// {
+//   path: process.env.NODE_ENV === 'test' ? '.env.test': '.env'
+// }
+
 module.exports = {
   "dialect": "sqlite",
-  storage: "./moveit.database.sqlite3",
+  storage: process.env.NODE_ENV === "test" ?
+    "./src/__tests__/moveit.test.database.sqlite3"
+    : "./src/database/moveit.database.sqlite3",
   define: {
     timestamps: true,
   },
