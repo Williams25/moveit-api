de9abd956c6bea8bffa32e379d004945b11d6f9d
Williams25 <william.souza.timao@hotmail.com>
2021-03-02 15:27:40 -0300

    Realizando test de integração

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -7,8 +7,8 @@ describe('Users', () => {
     const res = await req(app).post('/users').send({
       userName: 'Williams25',
       password: '123'
-    }) 
-    
+    })
+
     expect(res.status).toBe(201)
   })
 
@@ -16,20 +16,20 @@ describe('Users', () => {
     const res = await req(app).post('/users').send({
       userName: 'Williams25',
       password: '123'
-    }) 
-    
+    })
+
     expect(res.status).toBe(400)
   })
 
   it('Criar usuario sem corpo da requisição deve ocorrer status 400', async () => {
-    const res = await req(app).post('/users').send({}) 
-    
+    const res = await req(app).post('/users').send({})
+
     expect(res.status).toBe(400)
   })
 
   it('Listar ranking de usuarios deve ocorrer status 200', async () => {
-    const res = await req(app).get('/users/ranking') 
-    
+    const res = await req(app).get('/users/ranking')
+
     expect(res.status).toBe(200)
   })
 })
